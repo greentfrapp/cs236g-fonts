70c366b20dfe40df98a157fdfd2f94fc5a2ce297
Lim Swee Kiat <limsweekiat@gmail.com>
2021-03-08 14:34:08 +0800

    Fix bug

--- train_copy2.py
+++ train_copy2.py
@@ -58,7 +58,7 @@ def copy(gen, encoder_A, encoder_B, train_x_loader, train_y_loader, epoch, resiz
         if fixed_z is None:
             # fixed_z = gen.sample_z(BATCH_SIZE, device=device)
             fixed_z = encoder_A(real.unsqueeze(2))
-            fixed_z = encoder_B(z.squeeze(2))
+            fixed_z = encoder_B(fixed_z.squeeze(2))
             fixed_z = fixed_z.view(-1, 32*8*8)
         z = fixed_z.repeat(52, 1)  # shape = (bs*52, z_dim)
         glyph_one_hot = torch.eye(52).repeat(BATCH_SIZE, 1).to(device)  # shape = (52*bs, 52)
